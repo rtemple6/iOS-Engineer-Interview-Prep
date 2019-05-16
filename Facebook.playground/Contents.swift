eccdfc8dd50c9a477249f7efb8b38815234f8767
Ryan <rtemple@ramapo.edu>
2019-05-16 15:09:20 -0400

    Merged into 1 playground file

--- Facebook.playground/Contents.swift
+++ Facebook.playground/Contents.swift
@@ -26,3 +26,33 @@ class ValidPalindrome {
 
 let valid = ValidPalindrome()
 valid.validPalindrome("aabaaaaaad")
+
+//Find out the maximum sub-array of non negative numbers from an array.
+class Solution {
+    func maxset(_ A: inout [Int]) -> [Int] {
+        var result: [Int] = []
+        
+        var current: [Int] = []
+        var maxSum: Int = -1
+        var curSum: Int = 0
+        for (i, a) in A.enumerated() {
+            if a >= 0 {
+                current.append(a)
+                curSum += a
+            }
+            if a < 0 || i == A.count - 1 {
+                if curSum > maxSum {
+                    maxSum = curSum
+                    result = current
+                }
+                current = []
+                curSum = 0
+            }
+        }
+        return result
+    }
+}
+
+let solution = Solution()
+var array = [-1, -1, 3, 4]
+let max = solution.maxset(&array)
