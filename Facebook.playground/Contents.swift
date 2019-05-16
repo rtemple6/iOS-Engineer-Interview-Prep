d72354206f2f191b0673271d3dd8ed43eb2571ba
Ryan <rtemple@ramapo.edu>
2019-05-16 19:42:31 -0400

    Trapping rain water

--- Facebook.playground/Contents.swift
+++ Facebook.playground/Contents.swift
@@ -198,3 +198,40 @@ class AddBinary {
 }
 let addBinary = AddBinary()
 addBinary.addBinary("11", "1")
+//==============================================================================
+
+//==============================================================================
+//Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+class TrappingRainWater {
+    func trap(_ height: [Int]) -> Int {
+        guard height.count > 0 else {
+            return 0
+        }
+        
+        var leftPtr = 0, leftMax = height[0]
+        var rightPtr = height.count - 1, rightMax = height[height.count-1]
+        var water = 0
+        while leftPtr < rightPtr{
+            while leftMax <= rightMax && leftPtr < rightPtr{
+                if height[leftPtr] <= leftMax{
+                    water += leftMax - height[leftPtr]
+                    leftPtr += 1
+                }else{
+                    leftMax = height[leftPtr]
+                }
+            }
+            
+            while rightMax <= leftMax && leftPtr < rightPtr{
+                if height[rightPtr] <= rightMax{
+                    water += rightMax - height[rightPtr]
+                    rightPtr -= 1
+                }else{
+                    rightMax = height[rightPtr]
+                }
+            }
+        }
+        return water
+    }
+}
+let trapping = TrappingRainWater()
+trapping.trap([0,1,0,2,1,0,1,3,2,1,2,1])
