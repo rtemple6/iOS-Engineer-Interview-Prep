be1fd69fba8cdf2159e92e6a953832e72f751765
Ryan <rtemple@ramapo.edu>
2019-05-17 01:14:55 -0400

    Implemented Product of Array except self

--- Facebook.playground/Contents.swift
+++ Facebook.playground/Contents.swift
@@ -235,3 +235,29 @@ class TrappingRainWater {
 }
 let trapping = TrappingRainWater()
 trapping.trap([0,1,0,2,1,0,1,3,2,1,2,1])
+//==============================================================================
+
+//==============================================================================
+//Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+//Input:  [1,2,3,4]
+//Output: [24,12,8,6]
+class ProductOfArrayExceptSelf {
+    func productExceptSelf(_ nums: [Int]) -> [Int] {
+        var accumulator = 1
+        var result = Array(repeating: 1, count: nums.count)
+        
+        for (idx, n) in nums.enumerated() {
+            result[idx] = accumulator
+            accumulator *= n
+        }
+        
+        accumulator = 1
+        for i in stride(from: nums.count - 1, to: -1, by: -1){
+            result[i] *= accumulator
+            accumulator *= nums[i]
+        }
+        return result
+    }
+}
+let poaes = ProductOfArrayExceptSelf()
+poaes.productExceptSelf([1, 2, 3, 4])
