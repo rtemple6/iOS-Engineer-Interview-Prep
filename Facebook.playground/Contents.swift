5d389a7f7d1427b1b90833eb5d7751e24f3018fb
Ryan <rtemple@ramapo.edu>
2019-05-18 16:44:58 -0400

    Number of islands and Binary Tree Paths

--- Facebook.playground/Contents.swift
+++ Facebook.playground/Contents.swift
@@ -497,7 +497,6 @@ flattenBinary.flatten(root)
 //Input: [1,2,3,null,5,null,4]
 //Output: [1, 3, 4]
 
-
 class BinaryTreeRightSideView {
     // dfs(node, res, depth)
     // alwasy visit right node first
@@ -519,5 +518,116 @@ class BinaryTreeRightSideView {
         
         dfs(node.right, &res, depth + 1)
         dfs(node.left, &res, depth + 1)
+    }
+}
+//==============================================================================
+
+//==============================================================================
+//Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+//
+//Example 1:
+//
+//Input:
+//11110
+//11010
+//11000
+//00000
+//
+//Output: 1
+//Example 2:
+//
+//Input:
+//11000
+//11000
+//00100
+//00011
+//
+//Output: 3
+
+class NumberOfIslands {
+    func numIslands(_ grid: [[Character]]) -> Int {
+        var islands = 0
+        var grid = grid
+        for i in 0..<grid.count {
+            for j in 0..<grid[i].count {
+                if (grid[i][j] == "1"){
+                    islands += dfs(&grid, i, j)
+                }
+            }
+        }
+        return islands
+    }
+    
+    private func dfs(_ grid: inout [[Character]], _ i: Int, _ j: Int)->Int {
+        //Check if we are in bounds of matrix or if we are '0'
+        if (i < 0 || i >= grid.count || j < 0 || j >= grid[i].count || grid[i][j] == "0") {
+            return 0
+        }
+        
+        //'Sink' the island so we dont visit it again
+        grid[i][j] = "0"
+        
+        //traverse right, left, bottom, top
+        dfs(&grid, i + 1, j)
+        dfs(&grid, i - 1, j)
+        dfs(&grid, i, j + 1)
+        dfs(&grid, i, j - 1)
+        return 1
+    }
+}
+
+let numIslands = NumberOfIslands()
+numIslands.numIslands([["1"],["1"]])
+//==============================================================================
+
+//==============================================================================
+
+//Given a binary tree, return all root-to-leaf paths.
+//
+//Note: A leaf is a node with no children.
+//
+//Example:
+//
+//Input:
+//
+//    1
+//  /   \
+// 2     3
+// \
+//  5
+//
+//Output: ["1->2->5", "1->3"]
+
+//Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+
+class BinaryTreePaths {
+    func binaryTreePaths(_ root: TreeNode?) -> [String] {
+        var nodes = [TreeNode]()
+        var results = [String]()
+        dfs(root, &nodes, &results)
+        return results
+    }
+    
+    private func dfs(_ root: TreeNode?, _ arr: inout [TreeNode], _ results: inout [String]) {
+        guard let root = root else { return }
+        arr.append(root)
+        dfs(root.left, &arr, &results)
+        dfs(root.right, &arr, &results)
+        var tmpString = ""
+        for (idx, node) in arr.enumerated() {
+            if idx != arr.count - 1 {
+                tmpString += "\(node.val)->"
+            } else {
+                tmpString += "\(node.val)"
+            }
+        }
+        for x in results {
+            if x.contains(tmpString) {
+                arr.removeLast()
+                return
+            }
+        }
+        results.append(tmpString)
+        arr.removeLast()
     }
 }
