7030cbc8d8e06eea4711cad1d13b7a08ee23f282
Ryan <rtemple@ramapo.edu>
2019-05-18 13:19:41 -0400

    Added a validate binary search tree

--- Facebook.playground/Contents.swift
+++ Facebook.playground/Contents.swift
@@ -336,7 +336,7 @@ intToWords.numberToWords(1234567892)
 //==============================================================================
 
 //==============================================================================
-//Set operations
+//Set operations 
 var colors: Set<UIColor> = [.red, .blue, .purple]
 var colors1: Set<UIColor> = [.red, .black, .purple]
 
@@ -352,3 +352,80 @@ for color in colors1 {
 colors.intersection(colors1)
 //Removes all color in colors that colors1 contains
 colors.subtract(colors1)
+//==============================================================================
+
+//==============================================================================
+//Given a binary tree, determine if it is a valid binary search tree (BST).
+//
+//Assume a BST is defined as follows:
+//
+//The left subtree of a node contains only nodes with keys less than the node's key.
+//The right subtree of a node contains only nodes with keys greater than the node's key.
+//Both the left and right subtrees must also be binary search trees.
+//
+//
+//Example 1:
+//
+//  2
+// / \
+//1   3
+//
+//Input: [2,1,3]
+//Output: true
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+      self.val = val
+      self.left = nil
+      self.right = nil
+    }
+}
+class ValidateBinarySearchTree {
+    func isValidBST(_ root: TreeNode?) -> Bool {
+        guard let root = root else {
+            return true
+        }
+        
+        //Store each value of tree
+        var buffer:[Int] = []
+        visitBSTNode(root, &buffer)
+        
+        //Only one means true
+        if buffer.count == 1 {
+            return true
+        }
+        
+        //Ignore root
+        for i in 1 ..< buffer.count {
+            //If the current value is <= previous value
+            if buffer[i] <= buffer[i-1] {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    //InOrder traversal good for binary search trees
+    private func visitBSTNode(_ node: TreeNode, _ buffer: inout [Int]) {
+        if let left = node.left {
+            visitBSTNode(left, &buffer)
+        }
+        
+        buffer.append(node.val)
+        
+        if let right = node.right {
+            visitBSTNode(right, &buffer)
+        }
+    }
+}
+let validateBinary = ValidateBinarySearchTree()
+let root = TreeNode(2)
+let left = TreeNode(1)
+let right = TreeNode(3)
+root.left = left
+root.right = right
+validateBinary.isValidBST(root)
