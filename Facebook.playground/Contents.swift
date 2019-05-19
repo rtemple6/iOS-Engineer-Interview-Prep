023eaa3889a49a2f3f9ec1d52bffcc66ee436998
rtemple <ryan>
2019-05-18 21:38:23 -0400

    Diameter of a binary tree

--- Facebook.playground/Contents.swift
+++ Facebook.playground/Contents.swift
@@ -348,6 +348,7 @@ var colors1: Set<UIColor> = [.red, .black, .purple]
 for color in colors1 {
     print(color)
 }
+
 //Returns all color in colors1 that also in colors
 colors.intersection(colors1)
 //Removes all color in colors that colors1 contains
@@ -581,7 +582,6 @@ numIslands.numIslands([["1"],["1"]])
 //==============================================================================
 
 //==============================================================================
-
 //Given a binary tree, return all root-to-leaf paths.
 //
 //Note: A leaf is a node with no children.
@@ -629,5 +629,42 @@ class BinaryTreePaths {
         }
         results.append(tmpString)
         arr.removeLast()
+    }
+}
+//==============================================================================
+
+//==============================================================================
+//Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+//
+//Example:
+//Given a binary tree
+//
+//     1
+//    / \
+//   2   3
+//  / \
+// 4   5
+//Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+class DiameterOfBinaryTree {
+    func diameterOfBinaryTree(_ root: TreeNode?) -> Int {
+        if let r = root {
+            let lDiam = longestPath(r.left, currentLength: 0)
+            let rDiam = longestPath(r.right, currentLength: 0)
+            return max((lDiam + rDiam), diameterOfBinaryTree(r.left), diameterOfBinaryTree(r.right))
+        } else {
+            return 0
+        }
+    }
+    
+    private func longestPath (_ root: TreeNode?, currentLength: Int) -> Int {
+        if (root == nil) {
+            return currentLength
+        }
+        if (root!.left == nil && root!.right == nil) {
+            // Leaf
+            return currentLength + 1
+        } else {
+            return max(longestPath(root!.left, currentLength: currentLength + 1), longestPath(root!.right, currentLength: currentLength + 1))
+        }
     }
 }
